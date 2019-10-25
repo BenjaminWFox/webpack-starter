ac85de7cf921bddba4af9e0f108956ea19ad5898
Ben Fox <benjamin.fox@smashingideas.com>
2019-10-25 15:04:46 -0700

    Add better defaults for baseline project

--- webpack.config.resolve.js
+++ webpack.config.resolve.js
@@ -5,11 +5,6 @@ const path = require('path')
  */
 module.exports = {
   alias: {
-    Components: path.resolve(__dirname, 'src/components/'),
-    Navigation: path.resolve(__dirname, 'src/navigation/'),
     Classes: path.resolve(__dirname, 'src/classes/'),
-    Routes: path.resolve(__dirname, 'src/routes/'),
-    Redux: path.resolve(__dirname, 'src/redux/'),
-    Theme: path.resolve(__dirname, 'src/theme/'),
   },
 }
