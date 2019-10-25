09195efaec852ee1a17a6ceb3b351e38c1576241
Ben Fox <benjamin.fox@smashingideas.com>
2019-10-25 16:14:40 -0700

    Clean up dependencies

--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,6 @@
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-// const CopyWebpackPlugin = require('copy-webpack-plugin')
-const webpack = require('webpack')
 const resolve = require('./webpack.config.resolve')
 
 module.exports = {
